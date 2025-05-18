4d470b850aaeea1325def54c79d56e25061bf263
Tshwaro-T <tshwarothothela1@gmail.com>
2025-05-18 18:03:54 +0200

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\pacer\\Downloads\\flutter_windows_3.19.3-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\projects\\consulation moti" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\pacer\\Downloads\\flutter_windows_3.19.3-stable\\flutter"
+  "PROJECT_DIR=C:\\projects\\consulation moti"
+  "FLUTTER_ROOT=C:\\Users\\pacer\\Downloads\\flutter_windows_3.19.3-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\projects\\consulation moti\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\projects\\consulation moti"
+  "FLUTTER_TARGET=C:\\projects\\consulation moti\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\projects\\consulation moti\\.dart_tool\\package_config.json"
+)
